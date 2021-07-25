14a72b795ff1364fdd4c3ceeaac35e6b2b3fc24b
Jongveloper <59111836+Jongveloper@users.noreply.github.com>
2021-07-25 23:08:54 +0900

    📝Operator/DreamCodingByEllie(JS)

---
+++ JavaScript/operator.js
@@ -0,0 +1,170 @@
+// 1. String concatenation
+console.log("my" + "cat");
+console.log("1" + 2);
+console.log(`string literals: 1 + 2 = ${1 + 2}`);
+
+//2. Numeric operators
+console.log(1 + 1); // add
+console.log(1 - 1); // substract
+console.log(1 / 1); // divide
+console.log(1 * 1); // multiply
+console.log(5 % 2); // remainder
+console.log(2 ** 3); // exponentiation
+
+// 3. Increment and decrement operators
+let counter = 2;
+const preIncrement = ++counter;
+// counter = counter + 1;
+// preIncrement = counter;
+console.log(`preIncrement: ${preIncrement}, counter: ${counter}`);
+const postIncrement = counter++;
+// postIncrement = counter;
+// counter = counter + 1;
+console.log(`postIncrement: ${postIncrement}, counter: ${counter}`);
+
+// 4. Assignment operators
+let x = 3;
+let y = 6;
+x += y; // x = x + y;
+x -= y; // x = x - y;
+x *= y; // x = x * y;
+x /= y; // x = x / y;
+
+// 5. Comparison operators
+console.log(10 < 6); // less than
+console.log(10 <= 6); // less than or equal
+console.log(10 > 6); // greater than
+console.log(10 >= 6); // greater than or equal
+
+// 6. Logical operators: || (or), && (and), ! (not)
+const value1 = false;
+const value2 = 4 < 2;
+
+// || (or), finds the first truthy value
+console.log(`or: ${value1 || value2 || check()}`);
+
+// && (and), finds the first falsy value
+console.log(`and: ${value1 && value2 && check()}`);
+
+// often used to compress long if-statement
+// nullableObject && nullableObject.something
+
+function check() {
+  for (let i = 0; i < 10; i++) {
+    //wasting time
+    console.log("🤮");
+  }
+  return true;
+}
+
+// ! (not)
+console.log(!value1);
+
+// 7. Equality
+const stringFive = "5";
+const numberFive = 5;
+
+// == loose equality, with type conversion
+console.log(stringFive == numberFive); // true
+console.log(stringFive != numberFive); // false
+
+// === strict equality, no type conversion
+console.log(stringFive === numberFive); // flase
+console.log(stringFive !== numberFive); // true
+
+// object equality by reference
+const jong1 = { name: "jong" };
+const jong2 = { name: "jong" };
+const jong3 = jong1;
+console.log(jong1 == jong2); // false
+console.log(jong1 === jong2); // false
+console.log(jong1 === jong3); // true
+
+// eqaulity - puzzler
+console.log(0 == false); // true
+console.log(0 === false); // false
+console.log("" == false); // true
+console.log("" === false); // false
+console.log(null == undefined); // true
+console.log(null === undefined); // false
+
+// 8. Conditional operators: if
+// if, else if, else
+const name = "jong";
+if (name === "jong") {
+  console.log("Welcome, Jong!");
+} else if (name === "coder") {
+  console.log("You are amazing coder");
+} else {
+  console.log("unkwnon");
+}
+
+// 9. Ternary operator: ?
+// condition ? value1 : value2;
+console.log(name === "jong" ? "yes" : "no");
+
+// 10. Switch statement
+// use for multiple if checks
+// use for enum-like value check
+// use for multiple type checks in TS
+const browser = "IE";
+switch (browser) {
+  case "IE":
+    console.log("go away!");
+    break;
+  case "Chrome":
+  case "Firefox":
+    console.log("love you!");
+    break;
+  default:
+    console.log("same all!");
+    break;
+}
+
+// 11. Loops
+// while loop, while the condition is truthy,
+// body code is executed.
+let i = 3;
+while (i > 0) {
+  console.log(`while: ${i}`);
+  i--;
+}
+
+// do while loop, body code is executed first,
+// then check the condition.
+do {
+  console.log(`do while: ${i}`);
+  i--;
+} while (i > 0);
+// 블럭을 먼저 실행하고 i가 0인지 아닌지 검사한다.
+
+// for loop, for(begin; condition; step)
+for (i = 3; i > 0; i--) {
+  console.log(`for: ${i}`);
+}
+// begin을 처음 한번만 호출을 한다.
+
+for (let i = 3; i > 0; i = 1 - 2) {
+  // inline variable declaration
+  console.log(`inline variable for: ${i}`);
+}
+
+// nested loops
+for (let i = 0; i < 10; i++) {
+  for (let j = 0; j < 10; j++) {
+    console.log(`i: ${i}, j: ${j}`);
+  }
+}
+
+// break, continue
+for (let n = 0; n <= 10; n++) {
+  if (n % 2 === 1) {
+    continue;
+  }
+  console.log(`evenNumber: ${n}`);
+}
+
+for (let n = 0; n <= 10; n++) {
+  if (n > 8) break;
+  console.log(n);
+}
