033f8b831609fa9d6aa319869104bee3b7e5c330
Jongveloper <59111836+Jongveloper@users.noreply.github.com>
2021-07-27 21:56:53 +0900

    📝 JsonDreamCodingByEllie(JS)

---
+++ JavaScript/json.js
@@ -0,0 +1,43 @@
+// Json
+// JavaScript Object Notation
+
+// 1. Object to Json
+// stringfy(obj)
+let json = JSON.stringify(true);
+console.log(json);
+
+json = JSON.stringify(['apple', 'banana']);
+console.log(json);
+
+const rabbit = {
+  name: 'tori',
+  color: 'white',
+  size: 'null',
+  birthDate: new Date(),
+  jump: () => {
+    console.log(`${name} can jump`);
+  },
+};
+
+json = JSON.stringify(rabbit);
+console.log(json);
+
+json = JSON.stringify(rabbit, (key, value) => {
+  console.log(`key: ${key}, value: ${value}`);
+  return key === 'name' ? 'jong' : value;
+});
+console.log(json);
+
+// 2. Jsont to Object
+// parse(json)
+console.clear();
+json = JSON.stringify(rabbit);
+const obj = JSON.parse(json, (key, value) => {
+  console.log(`key: ${key}, value: ${value}`);
+  return key === 'birthDate' ? new Date(value) : value;
+});
+console.log(obj);
+rabbit.jump();
+
+console.log(rabbit.birthDate.getDate());
+console.log(obj.birthDate.getDate());
